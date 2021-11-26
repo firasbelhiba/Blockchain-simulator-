deadb5172fca474bcffafa6e9867fc06a34d791c
firasbelhiba <firas.belhiba@esprit.tn>
2021-11-27 00:54:09 +0100

    Creating web app + mine_block function

--- Blockchain.py
+++ Blockchain.py
@@ -57,6 +57,28 @@ class Blockchain:
             previous_block = block
             block_index += 1
         return True
- 
+    
+    
+# Creating a web app 
+app = Flask(__name__)
+
+# Creating Blockchain
+blockchain = Blockchain()
+
+# Mining a new block
+@app.route('/mine_block' , methods=['GET'])
+
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message' : 'Congratualtion , you just added your block to the blockchain' ,
+                'index' : block['index'],
+                'timestamp':block['timestamp'],
+                'proof':block['proof'],
+                'previous_hash':block['previous_hash']}
+    return jsonify(response), 200
              
     
