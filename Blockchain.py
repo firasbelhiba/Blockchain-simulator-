1ba23e98ab189dc9d582b4918716ff2830908f8b
firasbelhiba <firas.belhiba@esprit.tn>
2021-11-26 19:20:45 +0100

    Validate Blockchain function

--- Blockchain.py
+++ Blockchain.py
@@ -1,5 +1,5 @@
 """
-@author: Firas
+@author: Firas Belhiba
 """
 #Importing the libraries 
 import datetime
@@ -30,14 +30,33 @@ class Blockchain:
         new_proof = 1
         check_proof = False
         while check_proof is False:
-            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2)
-                                            .encode()
-                                            .hexdigest())
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode().hexdigest())
             if hash_operation[:4] == '0000':
                 check_proof = True
             else :
                 new_proof += 1
                 
         return new_proof
+    
+    def hash(self,block):
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+        
+    def is_chain_valid(self,chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode().hexdigest())
+            if hash_operation[:4] != '0000':
+                return False 
+            previous_block = block
+            block_index += 1
+        return True
+ 
              
     
